d4acc3d786e3f847a3075e8e732878edba908a0a
Andrew Elmore <andrewelmore93@gmail.com>
2021-02-04 15:12:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CSV-Converter-Demo
+
+## Overview
+
+The CSV Converter Demo is a demo version of an app I built to help a company, Trade Labels, rapidly and easily modify field names in CSV files. The company is migrating their database from Microsoft Access to Airtable, and in the process, they are changing field names. During the months-long transition, they will need to convert between their Access and Airtable names to use their various existing systems. This converter allows Trade Labels to do that in seconds rather than minutes. 
+
+## Libraries and Technologies
+This converter uses Node's File system, csvtojson, and json2csv libraries to modify and convert the file. The converter uses an Airtable base as the converter key. Finaly the client side of the app utilizes React.js.
+
+## Use
+
+A demo unconverted file is included in this repo, and is titled demo_data.csv. To use the converter, download this file, open the [live link](https://csv-converter-demo.herokuapp.com/), upload the file, and press convert. The file will be converted according to the Airtable base found [here](https://airtable.com/shrfWQF03aAhUnTJ9). 
+
+
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const converter = require("./routes/api/converter");
+const bodyParser = require("body-parser");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(fileUpload());
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => res.send("Hello Worlds"));
+app.use("/api/converter", converter)
+
+app.get("/download", (req, res) => {
+  console.log('route suceeded finally')
+  var options = {
+    root: path.join(__dirname, '/public')
+  };
+
+  var fileName = 'test.txt';
+  res.download(__dirname + '/public/test.txt', 'test.txt' )
+})
+
+
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
